7e2867110da6ca0d84e7f196b4cf5ca38674c158
danielepo <dnl.pozzobon@gmail.com>
2015-12-19 15:18:57 +0100

    Day5 first part

--- AdventOfCode/Day5DoesntHeHaveInternElvesForThis.fs
+++ AdventOfCode/Day5DoesntHeHaveInternElvesForThis.fs
@@ -16,5 +16,28 @@ let isVowl = function
 
 type Property = {
     NumberVowls: int;
-    HasTwice : bool
+    HasTwice : bool;
+    HasExcluded: bool;
     }
+
+let isGood (property:Property) = 
+    not property.HasExcluded &&
+    property.HasTwice &&
+    property.NumberVowls >= 3
+
+let isWordGood word =
+    let rec isWordGoodW (word:char list) (lastChar:char) (property:Property) =
+        match word with
+        | [] -> isGood property
+        | x::xs -> isWordGoodW xs x {
+            NumberVowls = (if isVowl x then property.NumberVowls + 1 else property.NumberVowls) ; 
+            HasTwice = property.HasTwice || twice (lastChar,x); 
+            HasExcluded = property.HasExcluded || excluded (lastChar,x)}
+    isWordGoodW (List.ofSeq word) ' ' {NumberVowls = 0; HasTwice = false; HasExcluded = false}
+
+let countGoods = 
+    "C:\Users\d.pozzobon\GitHubRepos\AdventOfCode\AdventOfCode\Day5Input.txt"
+    |> TextFileReader
+    |> Seq.map isWordGood
+    |> Seq.filter (fun x -> x)
+    |> Seq.length
